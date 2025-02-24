18741296a92bb412e5450cbb976c83b1b7b64788
Raja Kumar <teamaayushgupta@gmail.com>
2025-02-24 14:42:42 +0530

    initial commit

---
+++ exp3-LoginDetailsJava/src/main/java/com/mycompany/exp3/logindetailsjava/Exp3LoginDetailsJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exp3.logindetailsjava;
+
+/**
+ *
+ * @author teama
+ */
+public class Exp3LoginDetailsJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
